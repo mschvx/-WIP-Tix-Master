c1b862d57b71f1d798036e99e492700462951532
lee castro <149233214+mschvx@users.noreply.github.com>
2025-01-18 22:07:59 +0800

    Release Tix Master [FINAL]

---
+++ [FINAL] Tix Master/tix_loading.py
@@ -0,0 +1,171 @@
+# Importing necessary libraries
+import tkinter as tk
+from tkinter.ttk import *
+from tkinter import *
+from PIL import Image, ImageTk
+import random
+
+# Main class for loading page
+class TixLoading:
+    # Setting up basic properties of the window
+    def __init__(self, root, queue_number, selected_concert):
+        self.root = root
+        self.queue_number = queue_number
+        self.selected_concert = selected_concert 
+        self.root.geometry("1000x700")
+        self.root.resizable(False, False)
+        self.root.title("Loading...")
+        self.root.configure(bg="#d9e2f1")
+
+        # Initializing some necessary variables and putting the queues in a list for pop and delete
+        self.queue_list = []
+        self.current_queue = 1
+        # Adding the previous queues to a list
+        for i in range(self.queue_number):
+            self.queue_list.append(self.current_queue)
+            self.current_queue += 1
+
+        # Dictionary variable where numbers will be added later to a corresponding seat number
+        self.seating_arrangement = {
+            1: None, 2: None, 3: None,
+            4: None, 5: None, 6: None,
+            7: None, 8: None, 9: None,
+            10: None, 11: None, 12: None,
+            13: None, 14: None, 15: None,
+            16: None, 17: None, 18: None,
+            19: None, 20: None, 21: None,
+            22: None, 23: None, 24: None,
+            25: None, 26: None, 27: None,
+            28: None, 29: None, 30: None,
+            31: None, 32: None, 33: None,
+            34: None, 35: None, 36: None,
+            37: None, 38: None, 39: None,
+            40: None
+        }
+
+        # Placing the main design of the loading page
+        self.loading_title = tk.PhotoImage(file="Designs/Loading/Loading_Screen.png")
+        self.loading_title_label = tk.Label(self.root, image=self.loading_title, bg="#d9e2f1")
+        self.loading_title_label.image = self.loading_title
+        self.loading_title_label.place(x=0, y=0, relwidth=1, relheight=1)
+
+        # Initializing some elements for the animations
+        self.loading_one = Image.open("Designs/Loading/Loading_1.png")
+        self.loading_one_image = ImageTk.PhotoImage(self.loading_one)  
+        self.loading_two = Image.open("Designs/Loading/Loading_2.png")
+        self.loading_two_image = ImageTk.PhotoImage(self.loading_two) 
+        self.loading_three = Image.open("Designs/Loading/Loading_3.png")
+        self.loading_three_image = ImageTk.PhotoImage(self.loading_three)
+        self.loading_canvas = tk.Canvas(self.root, width=1000, height=700)
+        self.loading_canvas.pack()
+        self.loading_canvas.create_image(0, 0, anchor="nw", image=self.loading_title)
+
+        # Animation proper for the loading title above the page
+        self.current_loading_image = 0 # Track current image in the list below
+        self.loading_images = [self.loading_one_image, self.loading_two_image, self.loading_three_image]
+        self.current_loading_id = None # Tracks whatever is the current image
+        self.animate_loading_title()
+
+        # Placing the loading bar
+        style = Style()
+        style.theme_use('default')
+        style.configure("TProgressbar",
+                        troughcolor='#798ec4',  # Background color
+                        background='#151c4c')  # Color of the progress bar
+        self.bar = Progressbar(orient=HORIZONTAL, length=800, style="TProgressbar", mode='determinate')
+        self.bar.place(x=500, y=350, anchor="center", height=40)
+        self.bar['value'] = 0
+
+        # Placing the butterfly that moves along the loading bar
+        self.butterfly = Image.open("Designs/Loading/Loading_Butterfly.png")
+        self.butterfly_image = ImageTk.PhotoImage(self.butterfly)
+        self.butterfly_id = self.loading_canvas.create_image(100, 265, anchor="center", image=self.butterfly_image)
+
+        # Placing the percent below the loading bar
+        self.loading_percent = tk.Label(self.root, text="0%", bd=1, font=("Helvetica", 20), bg="#798ec4", fg="#d9e2f1", relief="raised")
+        self.loading_percent.place(x=500, y=400)
+
+        # Placing queue line message below the loading bar
+        self.queue_message = self.loading_canvas.create_text(500, 470, text=f"Your line number is {len(self.queue_list)}", font=("Helvetica", 20, "bold"), fill="#151c4c")
+
+        # Start the loading process
+        self.loading()
+
+    # Function for loading title animation
+    def animate_loading_title(self):
+        # Deletes the previous frame
+        if self.current_loading_id is not None:
+            self.loading_canvas.delete(self.current_loading_id)
+        # Logic for moving the animation
+        self.current_loading_id = self.loading_canvas.create_image(500, 80, anchor="center", image=self.loading_images[self.current_loading_image])
+        self.current_loading_image = (self.current_loading_image + 1) % len(self.loading_images)
+        self.root.after(500, self.animate_loading_title)
+
+    # Function for dequeue
+    def dequeue(self):
+        if self.queue_list:
+            self.queue_list.pop(0)  # Remove the first element from the queue
+        return self.queue_list
+    
+    # Function for the loading bar
+    def loading(self):
+        # Assigns a random empty seat with True
+        if self.queue_list:
+            assigned = False
+            # Also checks in case the random number generates a filled seat
+            while not assigned:
+                assign_seat_number = random.randint(1, 40)
+                if self.seating_arrangement[assign_seat_number] is None:
+                    self.seating_arrangement[assign_seat_number] = True
+                    # Saves the seats in a file (will be used for the concert seats proper)
+                    with open("assigned_seats.dat", "w") as assignedSeats:
+                        assignedSeats.write(str(self.seating_arrangement))
+                    assigned = True
+            self.dequeue()
+            self.bar['value'] += 100 / self.queue_number  # Update the progress bar
+            self.update_butterfly_position()  # Update butterfly position
+            self.update_percent(f"{int(self.bar['value'])}%")  # Update percent text
+            self.update_line(f"Your line number is {len(self.queue_list)}")
+            if len(self.queue_list) > 0:  # Continue loading if there are still items in the queue
+                self.root.after(1000, self.loading)
+            if len(self.queue_list) == 0:  # If loading is complete
+                self.open_seat_layout()
+            
+    def update_butterfly_position(self):
+            # Calculate the new position of the butterfly based on the progress bar's value
+            new_x = 100 + (self.bar['value'] / 100) * 800
+            self.loading_canvas.coords(self.butterfly_id, new_x, 265)
+
+    # Series of functions that change the texts below the loading bar (percent and queue number)
+    def update_percent(self, value):
+        self.loading_percent.config(text=value)
+        # Center the loading percent
+        self.loading_percent.place(x=500 - self.loading_percent.winfo_reqwidth() // 2, y=400)
+
+    def update_line(self, value):
+        self.loading_canvas.itemconfig(self.queue_message, text=value)
+        self.loading_canvas.coords(self.queue_message, 500, 470)
+
+    # Function for tracking which concert was selected
+    def open_seat_layout(self):
+        self.loading_canvas.pack_forget()  # Remove the canvas before transitioning
+        if self.selected_concert == "Elphaba Day 1":
+            from seats_ed1 import ED1Layout
+            ED1Layout(self.root)
+
+        if self.selected_concert == "Elphaba Day 2":
+            from seats_ed2 import ED2Layout
+            ED2Layout(self.root)
+
+        if self.selected_concert == "Glinda Day 1":
+            from seats_gd1 import GD1Layout
+            GD1Layout(self.root)
+
+        if self.selected_concert == "Glinda Day 2":
+            from seats_gd2 import GD2Layout
+            GD2Layout(self.root)
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = TixLoading(root, queue_number=10, selected_concert="Elphaba Day 1")
+    root.mainloop()
---
+++ [FINAL] Tix Master/tix_tickets.py
@@ -0,0 +1,121 @@
+# Importing necessary libraries
+import tkinter as tk
+
+# Main class for tickets page
+class tixTicket:
+
+    # Setting up basic properties of the window
+    def __init__(self, root):
+        self.root = root
+        self.root.geometry("1000x700")
+        self.root.resizable(False, False)
+        self.root.title("Ticket Generated!")
+        self.root.configure(bg="#d9e2f1")
+
+        # Initializing empty variables for later use
+        self.concert_name = ''
+        self.name = ''
+        self.seat_numbers_text = ''
+        self.seat_details_text = ''
+        self.price_text = ''
+
+        # Placing the main design of the catalogue page
+        self.ticket_title = tk.PhotoImage(file="Designs/Ticket/Tix_Ticket.png")
+        self.ticket_canvas = tk.Canvas(self.root, width=1000, height=700)
+        self.ticket_canvas.pack()
+        self.ticket_canvas.create_image(0, 0, anchor="nw", image=self.ticket_title)
+
+        # Placing the return button
+        self.back = tk.PhotoImage(file="Designs/Ticket/Return_Button.png")
+        self.back_button = tk.Button(self.root, image=self.back, relief="raised", command=self.open_title)
+        self.back_button.image = self.back
+        self.back_button.place(x=830, y=625, anchor="center")
+        self.back_hover = tk.PhotoImage(file="Designs/Ticket/Return_Button_Hover.png")
+        self.back_button.bind("<Enter>", self.on_hover_back)
+        self.back_button.bind("<Leave>", self.on_leave_back)
+        self.back_button.image = self.back_hover
+
+        # Loading cart
+        self.load_cart()
+
+        # Placing the main concert name above the ticket
+        self.queue_message = self.ticket_canvas.create_text(360, 271, text=f"Ticket for {self.concert_name} Concert", font=("Helvetica", 20, "bold"), fill="#151c4c", anchor="center") # anchor center like center alignment in ms word
+
+        # Placing ty message
+        self.ref_id = self.ticket_canvas.create_text(360, 350, text="Ticket generated!", font=("Helvetica", 20, "bold"), fill="#151c4c", anchor="center")
+
+        # Placing the customer name
+        self.ticket_holder = self.ticket_canvas.create_text(240, 396, text=f"{self.name}", font=("Helvetica", 18, "bold"), fill="#edebe7", anchor="w") # align left
+    
+        # Placing seat num
+        self.seat_numbers = self.ticket_canvas.create_text(250, 435, text=self.seat_numbers_text, font=("Helvetica", 18, "bold"), fill="#edebe7", anchor="w") # align left
+
+        # Placing seat details
+        self.seat_details = self.ticket_canvas.create_text(225, 475, text=self.seat_details_text, font=("Helvetica", 18, "bold"), fill="#edebe7", anchor="w") # align left
+
+        # Placing price
+        self.price = self.ticket_canvas.create_text(135, 516, text=self.price_text, font=("Helvetica", 18, "bold"), fill="#edebe7", anchor="w") # align left
+
+        # Placing Glinda's picture
+        if self.concert_name == "Glinda - Day 1" or self.concert_name == "Glinda - Day 2":
+            self.glinda_image = tk.PhotoImage(file="Designs/Ticket/Glinda_Ticket.png")
+            self.glinda_image_label = tk.Label(self.root, image=self.glinda_image, bg="#d9e2f1")
+            self.glinda_image_label.image = self.glinda_image
+            self.glinda_image_label.place(x=840, y=330, anchor="center")
+
+        # Placing Elphaba's picture
+        if self.concert_name == "Elphaba - Day 1" or self.concert_name == "Elphaba - Day 2":
+            self.elphaba_image = tk.PhotoImage(file="Designs/Ticket/Elphaba_Ticket.png")
+            self.elphaba_image_label = tk.Label(self.root, image=self.elphaba_image, bg="#d9e2f1")
+            self.elphaba_image_label.image = self.elphaba_image
+            self.elphaba_image_label.place(x=840, y=330, anchor="center")
+
+    # Series of functions for the hover effects of each button
+    def on_hover_back(self, event):
+        self.back_button.config(image=self.back_hover)
+
+    def on_leave_back(self, event):
+        self.back_button.config(image=self.back)
+
+    # Function for restarting (yay!) going back to the main menu
+    def open_title(self):
+        from tix_master import TixMaster
+        self.ticket_canvas.pack_forget()  # Remove the canvas before transitioning
+        TixMaster(self.root)
+    
+    # Function for loading the cart
+    def load_cart(self):
+        with open("tix_cart.dat", "r") as tixCart:
+            load = tixCart.readlines()
+            self.concert_name = load[0].strip()
+            self.items = []
+
+            for line in load[1:-1]:
+                seat_number, rest = line.strip().split(' - ')
+                seat_detail, price = rest.split(':')
+                if seat_detail == "Lower Box":
+                    seat_detail = "LB"
+                if seat_detail == "Upper Box":
+                    seat_detail == "UB"
+                if seat_detail == "Right Balcony":
+                    seat_detail = "RB"
+                if seat_detail == "Left Balcony":
+                    seat_detail = "LB"
+                self.items.append({
+                    'seat_number': seat_number,
+                    'seat_detail': seat_detail,
+                    'price': int(price)
+                })
+
+            self.name = load[-1].strip() # Get last line (name)
+
+        # Extracting seat numbers, details, and total price
+        self.seat_numbers_text = ", ".join([item['seat_number'] for item in self.items])
+        self.seat_details_text = ", ".join([item['seat_detail'] for item in self.items])
+        self.price_text = "₱" + str(sum(item['price'] for item in self.items))
+
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = tixTicket(root)
+    root.mainloop()
